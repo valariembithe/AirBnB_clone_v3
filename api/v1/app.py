979822b28c250c78a415284c934a87bb081f81e3
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-10-30 02:31:24 +0300

    edit app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-    Creating an api
+App.py
 """
-import models
+from flask import Flask
 from os import getenv
-from models import storage
 from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
-app.register_blue_print(app_views)
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(exception):
+    """Closes the storage on teardown."""
     storage.close()
 
-
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
-    port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else '5000'
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
