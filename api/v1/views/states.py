05a87c2400cd46c11f34a1cfbf372201d5c82a10
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-10-30 23:38:24 +0300

    Updated states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,11 +7,13 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects."""
     states = [state.to_dict() for state in storage.all(State).values()]
     return jsonify(states)
+
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
@@ -21,7 +23,9 @@ def get_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """Deletes a State object."""
     state = storage.get(State, state_id)
@@ -31,18 +35,20 @@ def delete_state(state_id):
     storage.save()
     return jsonify({}), 200
 
+
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Creates a State."""
     data = request.get_json()
     if data is None:
-        abort(400, "Not a JSON")
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
     if 'name' not in data:
-        abort(400, "Missing name")
+        return make_response(jsonify({'error': 'Missing name'}), 400)
     new_state = State(**data)
     storage.new(new_state)
     storage.save()
     return jsonify(new_state.to_dict()), 201
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
@@ -52,7 +58,7 @@ def update_state(state_id):
         abort(404)
     data = request.get_json()
     if data is None:
-        abort(400, "Not a JSON")
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
