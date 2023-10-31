4b0a9e03761762621ff7e4412c7786b15311a12f
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-11-01 02:05:57 +0300

    used CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,12 +6,13 @@ from flask import Flask, jsonify, make_response
 from os import getenv
 from api.v1.views import app_views
 from models import storage
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def teardown(exception):
@@ -23,7 +24,6 @@ def teardown(exception):
 def not_found(error):
     """Handles 404 errors and returns a JSON-formatted response."""
     return make_response(jsonify({"error": "Not found"}), 404)
-
 
 
 if __name__ == "__main__":
